109938e11be5bf1b35082726befaa9aee65bbd35
rafawelsh <mrrafael1912@gmail.com>
2021-07-25 09:31:20 -0700

    filtering students by their name

--- src/students/StudentsView.js
+++ src/students/StudentsView.js
@@ -1,16 +1,24 @@
 import React, {useState} from 'react'
 import IndividualStudent from './IndividualStudent'
+import SearchBar from '../searchBar/SearchBar'
 
 function StudentsView(students) {
     const [searchTerm, setSearchTerm] = useState('');
 
     if (!students.students) return null
-
     students = students.students
 
     return (
         <div>
-            {students.map(student => {
+            <input type="text" placeholder="Search.." onChange={(event) => {setSearchTerm(event.target.value)}}/>
+
+            {students.filter((search) => {
+                if (searchTerm === '') {
+                    return search
+                } else if (search.firstName.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || search.lastName.toLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                    return search
+                }
+            }).map(student => {
                 return (
                     <IndividualStudent student={student} />
                 )
